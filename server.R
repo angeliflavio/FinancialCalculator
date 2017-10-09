ceb926fd4df35190d46da43a06d99b30f3bacd20
Flavio Angeli <angeli.flaviouk@gmail.com>
2017-10-09 14:28:55 +0100

    added debt

--- server.R
+++ server.R
@@ -1,14 +1,15 @@
 library(shiny)
 library(ggplot2)
 library(plotly)
+library(FinancialMath)
 
 
 shinyServer(function(input,output){
     
-    output$futurepresent <- renderText({
+    output$futurepresent <- renderValueBox({
         years <- ifelse(input$futurepresent=='f',input$years,(-input$years))
         value <- (input$value*(1+(input$interest/100))^(years))
-        value
+        valueBox('Result',value = round(value,2),color = 'navy')
     })
     
     output$chartfuturepresent <- renderPlotly({
@@ -18,11 +19,50 @@ shinyServer(function(input,output){
         i <- input$interest/100
         d <- data.frame(Years=y,Value=numeric(length(y)))
         d$Value <- v*((1+i)^(d$Years))
-        plot_ly(x=d$Years,y=d$Value,type = 'bar',mode='markers',marker=list(color='navy'))
+        plot_ly(x=d$Years,y=d$Value,type = 'bar',mode='markers',marker=list(color='navy'),
+                hoverinfo='y+name',name='Value') %>% 
+            layout(yaxis=list(range=c(min(d$Value)*0.9,max(d$Value)*1.05)))
     })
+    
+    
+    output$irr <- renderValueBox({
+        irr <- (input$finalvalue/input$initialvalue)^(1/input$yearsirr)-1
+        valueBox('IRR',value = paste(round(irr*100,2),'%',sep = ' '),color = 'navy')
+    })
+    
+    debt <- reactive({
+        n <- 12/as.integer(input$debtfrequency)
+        amort.table(Loan = input$debt,n = n*input$yearsdebt,i = input$interestdebt/100,pf = n)
+    })
+    
+    output$tabledebt <- renderDataTable({
+        d <- debt()
+        d$Schedule
+    })
+    
+    output$chartdebt <- renderPlotly({
+        c <- debt()
+        c <- as.data.frame(c$Schedule)
+        colnames(c) <- c('Time','Peyment','Interest','Principal','Balance')
+        plot_ly(c) %>% 
+            add_trace(x=~Time,y=~Principal,type='bar',name='Principal',marker=list(color='light-blue'),
+                      hoverinfo='y+name') %>% 
+            add_trace(x=~Time,y=~Interest,type='bar',name='Interest',color=I('red'),
+                      hoverinfo='y+name') %>% 
+            add_trace(x=~Time,y=~Balance,type='scatter',mode='lines+markers',name='Balance',yaxis='y2',
+                      hoverinfo='y+name',color=I('black'),line=list(width=3),
+                      marker=list(symbol='circle')) %>%  
+            layout(yaxis=list(side='left',title='Principal + Interest',showgrid=F,zeroline=F),
+                   barmode='stack',
+                   yaxis2=list(side='right',title='Debt Balance',overlaying='y',showgrid=F,zeroline=F))
+    })
+    
+    #output$debtsummary <- 
     
 })
 
+
+  
 
 
 
--- ui.R
+++ ui.R
@@ -1,11 +1,15 @@
 library(shiny)
 library(shinyBS)
 library(shinydashboard)
-
+library(plotly)
 
 dashboardPage(
-    dashboardHeader(title = 'Financial Calculator'
-                    ),
+    dashboardHeader(title = 'Financial Calculator',
+                    dropdownMenu(type = 'notification',headerText = ' ',
+                                 icon = img(src='github.png',width='17px'),badgeStatus = NULL,
+                                 notificationItem('GitHub Source Code',
+                                                  href = 'https://github.com/angeliflavio/FinancialCalculator',
+                                                  icon = icon('github')))),
     dashboardSidebar(
         sidebarMenu(
             menuItem("Future or Present Value", tabName = "present"),
@@ -27,28 +31,39 @@ dashboardPage(
                             numericInput('interest','Interest',value = 5)),
                         box(width = 9,
                             verticalLayout(
-                                box(title='Result',background = 'navy',width = 4,
-                                    textOutput('futurepresent')),
-                                box(title = 'Timeline',
+                                valueBoxOutput('futurepresent',width = 6),
+                                box(title = 'Chart',
                                     plotlyOutput('chartfuturepresent'),width = 12)
                             )
                             )
                     )),
-            tabItem('future',
-                    sidebarLayout(
-                        sidebarPanel(),
-                        mainPanel()
-                    )),
+            tabItem('future'
+                    ),
             tabItem('irr',
-                    sidebarLayout(
-                        sidebarPanel(),
-                        mainPanel()
-                    )),
+                    fluidRow(
+                        box(title = 'Options',background = 'light-blue',width = 3,collapsible = T,
+                            numericInput('initialvalue','Initial Value',value = 1000),
+                            numericInput('finalvalue','Final Value',value = 1500),
+                            numericInput('yearsirr','Years',value = 5)),
+                        box(width = 9,
+                            valueBoxOutput('irr',width = 6))
+                    )
+                   ),
             tabItem('debt',
-                    sidebarLayout(
-                        sidebarPanel(),
-                        mainPanel()
-                    ))
+                    fluidRow(
+                        box(title = 'Options',background = 'light-blue',width = 3,collapsible = T,
+                            numericInput('debt','Debt Amount',value = 1000),
+                            numericInput('yearsdebt','Duration (years)',value = 5),
+                            numericInput('interestdebt','Annual Interest (in %)',value = 5),
+                            selectInput('debtfrequency','Payment Frequency (months)',
+                                        choices = seq(1:12),selected = 1)),
+                        tabBox(width = 9,
+                            tabPanel('Table',dataTableOutput('tabledebt')),
+                            tabPanel('Chart',plotlyOutput('chartdebt')),
+                            tabPanel('Summary')
+                        )
+                    )
+                    )
         )
     )
     
