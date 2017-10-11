b56b8c626c6e5d64ff6e0f3e6cf7302be7ad020e
Flavio Angeli <angeli.flaviouk@gmail.com>
2017-10-11 20:25:43 +0100

    add flows

--- server.R
+++ server.R
@@ -79,6 +79,97 @@ shinyServer(function(input,output){
         valueBox('Total Paid',value = sum(t$Payment),color = 'navy')
     })
     
+
+    store <- reactiveValues()
+    store$variable <- data.frame(N=numeric(0),Months=numeric(0),Flow=numeric(0),
+                                 Year=numeric(0),Present=numeric(0),Final=numeric(0))
+    store$constant <- data.frame(N=numeric(0),Months=numeric(0),Flow=numeric(0),
+                                 Year=numeric(0),Present=numeric(0),Final=numeric(0))
+    
+   
+    observeEvent(input$insert,{
+            i <- input$discountrate/100
+            if (input$constantvariable=='Constant'){
+                n <- input$numberflows
+                m <- seq(1:n)*as.integer(input$flowsfrequency)
+                store$constant <- data.frame(N=seq(1:n),Months=m,Flow=rep(input$constantflow,n))
+                store$constant$Year <- store$constant$Months/12
+                store$constant$Present <- round(store$constant$Flow*(1+i)^(-store$constant$Year),2)
+                store$constant$Final <- round(store$constant$Flow*(1+i)^(rev(store$constant$Year)-1),2)
+            }
+            if (input$constantvariable=='Variable'){
+                to <- input$to
+                from <- input$from
+                if (to>nrow(store$variable)){
+                    add <- data.frame(N=numeric(to-nrow(store$variable)),Months=NA,Flow=NA,
+                                      Year=NA,Present=NA,Final=NA)
+                    store$variable <- rbind(store$variable,add)
+                }
+                store$variable$Flow[from:to] <- input$variableflow
+                store$variable$N <- seq(1:nrow(store$variable))
+                store$variable$Months <- store$variable$N*as.integer(input$flowsfrequency)
+                store$variable$Year <- store$variable$Months/12
+                store$variable$Present <- round(store$variable$Flow*(1+i)^(-store$variable$Year),2)
+                store$variable$Final <- round(store$variable$Flow*(1+i)^(rev(store$variable$Year)-1),2)
+            }
+    })
+    
+    output$tableflows <- renderDataTable({
+        if (input$constantvariable=='Constant'){d <- store$constant}
+        if (input$constantvariable=='Variable'){d <- store$variable}
+        d
+    })
+    
+    observeEvent(input$delete,{
+        if (input$constantvariable=='Constant'){
+            store$constant <- data.frame(N=numeric(0),Months=numeric(0),Flow=numeric(0))
+        }
+        if (input$constantvariable=='Variable'){
+            from <- input$from
+            to <- input$to
+            store$variable <- store$variable[-c(from:to),]
+            store$variable$N <- seq(1:nrow(store$variable))
+            store$variable$Months <- store$variable$N*as.integer(input$flowsfrequency)
+        }
+    })
+    
+    output$finalvalue <- renderValueBox({
+        if (input$constantvariable=='Constant'){
+            s <- sum(store$constant$Final)
+        }
+        if (input$constantvariable=='Variable'){
+            s <- sum(store$variable$Final)
+        }
+        valueBox('Final Value',value = round(s),color = 'navy')
+    })
+    
+    output$presentvalue <- renderValueBox({
+        if (input$constantvariable=='Constant'){
+            f <- sum(store$constant$Present)
+        }
+        if (input$constantvariable=='Variable'){
+            f <- sum(store$variable$Present)
+        }
+        valueBox('Present Value',value = round(f),color = 'navy')
+    })
+    
+    output$chartflow <- renderPlotly({
+        if (input$constantvariable=='Constant'){
+            c <- store$constant
+        }
+        if (input$constantvariable=='Variable'){
+            c <- store$variable
+        }
+        c$cumsum <- cumsum(c$Flow) 
+        plot_ly(x=c$Months,y=c$Flow,type = 'bar',name='Flow') %>% 
+            add_trace(x=c$Months,y=c$cumsum,type='scatter',mode='line+markers',name='Cumulative',
+                      yaxis='y2',line=list(width=3),color=I('black'),
+                      markers=list(symbol='circle')) %>% 
+            layout(yaxis=list(side='left',title='Flow',showgrid=F,zeroline=F),
+                   yaxis2=list(side='right',title='Cumulative',overlaying='y',showgrid=F,zeroline=F),
+                   xaxis=list(title='Months'))
+    })
+    
 })
 
 
--- ui.R
+++ ui.R
@@ -37,7 +37,44 @@ dashboardPage(
                             )
                             )
                     )),
-            tabItem('future'
+            tabItem('flow',
+                    fluidRow(
+                        box(title = 'Options',background = 'light-blue',width = 3,collapsible = T,
+                            numericInput('discountrate','Interest Rate (%)',value = 5),
+                            numericInput('numberflows','Number of Flows',value = 12),
+                            selectInput('flowsfrequency','Flows Frequency (months)',
+                                        choices = seq(1,12),selected = 12),
+                            br(),
+                            radioButtons('constantvariable','Cash Flow',
+                                         choices = c('Constant','Variable'),
+                                         inline = T,selected = 'Constant'),
+                            conditionalPanel(
+                                condition = "input.constantvariable == 'Constant'",
+                                numericInput('constantflow','Constant Flow',value = 1000)
+                            ),
+                            conditionalPanel(
+                                condition = "input.constantvariable == 'Variable'",
+                                fluidRow(
+                                    column(width=6,numericInput('from','From',value = 1,min = 1,max = 100,step = 1)),
+                                    column(width=6,numericInput('to','To',value = 1,min = 1,max = 100,step = 1))
+                                ),
+                                numericInput('variableflow','Variable Flow',value = 1000)
+                            ),
+                            actionButton('delete','Delete',
+                                         style="color:white;background-color:midnightblue;border-color:midnightblue"),
+                            actionButton('insert','Update',
+                                         style="color:white;background-color:midnightblue;border-color:midnightblue")
+                        ),
+                        tabBox(width = 9,
+                               tabPanel('Flows Table',
+                                        flowLayout(
+                                            valueBoxOutput('presentvalue',width = 12),
+                                            valueBoxOutput('finalvalue',width = 12)
+                                        ),
+                                        dataTableOutput('tableflows')
+                                        ),
+                               tabPanel('Chart',plotlyOutput('chartflow')))
+                    )
                     ),
             tabItem('irr',
                     fluidRow(
